9b4f429c247e242b3ecb88e4b4d1a07296a9dac8
h4nz1 <xxxh4nz1xxx@gmail.com>
2025-06-06 23:33:06 +0700

    Функция удаления гласных

--- TextToolApp/Program.cs
+++ TextToolApp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите текст: ");
+        string text = Console.ReadLine();
+
+        Console.WriteLine("\nВыберите действие:");
+        Console.WriteLine("3. Удалить гласные");
+
+        Console.Write("Ваш выбор: ");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "3":
+                Console.WriteLine("Без гласных: " + RemoveVowels(text));
+                break;
+            default:
+                Console.WriteLine("Неверный выбор.");
+                break;
+        }
+    }
+
+    static string RemoveVowels(string text)
+    {
+        string vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯaeiouAEIOU";
+        string result = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            bool isVowel = false;
+            for (int j = 0; j < vowels.Length; j++)
+            {
+                if (text[i] == vowels[j])
+                {
+                    isVowel = true;
+                    break;
+                }
+            }
+            if (!isVowel)
+            {
+                result += text[i];
+            }
+        }
+        return result;
+    }
+}
