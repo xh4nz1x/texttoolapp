224130169906ce5db3fd618f74a9b29acf095f00
h4nz1 <xxxh4nz1xxx@gmail.com>
2025-06-06 23:28:03 +0700

    Функция инвертирования текста

--- TextToolApp/Program.cs
+++ TextToolApp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите текст: ");
+        string text = Console.ReadLine();
+
+        Console.WriteLine("\nВыберите действие:");
+        Console.WriteLine("2. Инвертировать текст");
+
+        Console.Write("Ваш выбор: ");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "2":
+                Console.WriteLine("Инвертированный текст: " + ReverseText(text));
+                break;
+            default:
+                Console.WriteLine("Неверный выбор.");
+                break;
+        }
+    }
+
+    static string ReverseText(string text)
+    {
+        char[] reversed = new char[text.Length];
+        for (int i = 0; i < text.Length; i++)
+        {
+            reversed[i] = text[text.Length - 1 - i];
+        }
+        return new string(reversed);
+    }
+}
