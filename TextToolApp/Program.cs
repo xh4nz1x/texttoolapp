de45096e715239ba33af5673f4ff05ef4671076b
h4nz1 <xxxh4nz1xxx@gmail.com>
2025-06-06 23:35:58 +0700

    Функция изменения регистра

--- TextToolApp/Program.cs
+++ TextToolApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите текст: ");
+        string text = Console.ReadLine();
+
+        Console.WriteLine("\nВыберите действие:");
+        Console.WriteLine("4. Изменить регистр");
+
+        Console.Write("Ваш выбор: ");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "4":
+                Console.WriteLine("1. В нижний регистр\n2. В верхний регистр");
+                string subChoice = Console.ReadLine();
+                if (subChoice == "1") Console.WriteLine(text.ToLower());
+                else if (subChoice == "2") Console.WriteLine(text.ToUpper());
+                else Console.WriteLine("Неверный выбор.");
+                break;
+            default:
+                Console.WriteLine("Неверный выбор.");
+                break;
+        }
+    }
+}
